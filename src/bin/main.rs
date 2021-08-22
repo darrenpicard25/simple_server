25b2b98878f12def7ebeb3b99a2965a0935fae5c
Darren Picard <darren.picard@neofinancial.com>
2021-08-22 17:16:49 -0600

    Finished project with multiple threads

---
+++ src/bin/main.rs
@@ -0,0 +1,48 @@
+use simple_server::ThreadPool;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting Down");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "html/hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "html/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        content.len(),
+        content
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
